80c5ef311e7f5d1d186262e100f549a0b2db08e5
Andrew <arbanuelos@gmail.com>
2025-08-14 21:12:07 -0700

    add variables lesson

---
+++ variables.py
@@ -0,0 +1,13 @@
+Name = ("Andrew")
+Age = (24)
+Height = (69)
+Weight = (193)
+#ok so previous i just listed the same variables and used quotations and got what was listed then i erased them and got what i wanted good to know, all from memory no lesson or vid watched
+
+print(Name, Age, Height, Weight)
+
+
+#You can think of a variable as a box that contains some information. 
+city = ("Altadena")
+founded_in = (1920)
+print(city, founded_in)
